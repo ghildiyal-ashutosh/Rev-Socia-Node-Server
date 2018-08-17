53aaf95b7145786bfd1033a25373f6fd5092b0ca
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-17 09:38:27 -0400

    server.js added

---
+++ Node-Server/server.js
@@ -0,0 +1,48 @@
+var express = require('express')
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+const mongoose = require ('mongoose');
+mongoose.connect('mongodb://127.0.0.1/rev-socia');
+
+
+app.use(function (req,res,next) {
+
+    var allowedOrigins = ['http://localhost:4200'];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any string',
+    cookie:{
+        maxAge: 30 * 60 * 1000
+    },
+    rolling : true
+}
+));
+
+
+
+
+app.get('/', function (req,res) {
+    res.send('An Online Platform for reviews');
+
+});
+
+
+app.listen(3000);
+
+// app.listen(process.env.PORT || 3000);
+
+//  app.listen(process.env.PORT || 3000) ;
